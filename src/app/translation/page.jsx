46762f66bc185a41fe4fd43045fd3cf6fdba37ea
dung.do <dung.do@techup.vn>
2024-09-01 19:08:12 +0300

    [add] guide cho dau xanh

---
+++ src/app/translation/page.jsx
@@ -0,0 +1,14 @@
+export default function Shorts() {
+  return (
+    <main>
+      <h1 class="font-bold">
+        Thư mục app/translation tương ứng với đường dẫn url/translation
+      </h1>
+      <div class="flex flex-col">
+        <a href="/translation/1">Translation 1</a>
+        <a href="/translation/2">Translation 2</a>
+        <a href="/translation/3">Translation 3</a>
+      </div>
+    </main>
+  );
+}
