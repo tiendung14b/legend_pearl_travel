b109d139bbdd92311bbfa7c3ab5ead1c101ac3b6
dung.do <dung.do@techup.vn>
2024-09-11 17:10:52 +0700

    [add] add languge for video

--- src/app/components/common/banner.jsx
+++ src/app/components/common/banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner({ children, label }) {
   return (
-    <div class="relative h-[175px] w-full rounded-xl linear-gradient">
-      <p class="absolute bottom-[15px] left-[38px] text-[46px] font-[700] text-white">
+    <div class="relative h-[140px] w-full rounded-xl linear-gradient">
+      <p class="absolute bottom-[15px] left-[38px] text-[36px] font-[700] text-white">
         {label || "Banner"}
       </p>
       <div class="absolute bottom-0 right-[140px]">{children}</div>
--- src/app/components/homepage/page2.jsx
+++ src/app/components/homepage/page2.jsx
@@ -1,6 +1,70 @@
 import Banner from "components/common/banner";
+import { useState } from "react";
+
+function LanguagesSelection({ languages, addLanguage }) {
+  return (
+    <div class="absolute bottom-0 translate-y-1/2 left-full flex flex-col mt-2 h-[130px] bg-white shadow-lg overflow-y-scroll hover:*:bg-[#F37B8F26] hover:*:text-red-600 *:cursor-pointer">
+      {languages.map((language) => (
+        <span
+          class=" text-black rounded-md px-2 py-1 text-[12px] "
+          key={language.languageCode}
+          onClick={() => addLanguage(language)}
+        >
+          {language.language}
+        </span>
+      ))}
+    </div>
+  );
+}
 
 export default function Page2({ onChangePage, data }) {
+  const [open, setOpen] = useState(false);
+  const [currLanguage, setCurrLanguage] = useState("en");
+
+  const [languages, setLanguages] = useState([
+    { language: "English", languageCode: "en" },
+    { language: "Spanish", languageCode: "es" },
+  ]);
+
+  const addLanguage = async (language) => {
+    if (languages.length >= 5) return;
+    try {
+      const res = await fetch("http://127.0.0.1:8000/translate", {
+        url:
+          "https://www.youtube.com/watch?v=" + data.snippet.resourceId.videoId,
+        language: language.languageCode,
+      });
+      const result = await res.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+    setLanguages([...languages, language]);
+    setCurrLanguage(language.languageCode);
+    setOpen(false);
+  };
+
+  const availableLanguage = [
+    { language: "English", languageCode: "en" },
+    { language: "Spanish", languageCode: "es" },
+    { language: "French", languageCode: "fr" },
+    { language: "German", languageCode: "de" },
+    { language: "Italian", languageCode: "it" },
+    { language: "Portuguese", languageCode: "pt" },
+    { language: "Polish", languageCode: "pl" },
+    { language: "Turkish", languageCode: "tr" },
+    { language: "Russian", languageCode: "ru" },
+    { language: "Dutch", languageCode: "nl" },
+    { language: "Czech", languageCode: "cs" },
+    { language: "Arabic", languageCode: "ar" },
+    { language: "Chinese", languageCode: "zh-cn" },
+    { language: "Japanese", languageCode: "ja" },
+    { language: "Hungarian", languageCode: "hu" },
+    { language: "Korean", languageCode: "ko" },
+    { language: "Hindi", languageCode: "hi" },
+  ];
+
   return (
     <div class="flex flex-col h-[95vh]">
       <Banner label="My Projects">
@@ -25,12 +89,55 @@ export default function Page2({ onChangePage, data }) {
             allowfullscreen
           ></iframe>
           <div class="flex flex-col gap-4 border-[1px] border-slate-400 h-[80%] w-full rounded-lg px-6 py-5">
-            <button class="w-full rounded-sm text-[#f00] text-[14px] font-[700] py-2 px-[74px] bg-[#F37B8F26]">
+            <button class="w-full rounded-sm text-[#f00] text-[14px] font-[700] py-2 text-center bg-[#F37B8F26]">
               Create short
             </button>
             <div class="flex flex-col">
-              <strong class="text-[14px] font-[700]">Title</strong>
+              <strong class="text-[16px] font-[700]">Title</strong>
               <span>{data.snippet.title}</span>
+            </div>
+            <div class="flex flex-col">
+              <strong class="text-[16px] font-[700]">Date</strong>
+              <span>
+                {new Date(data.snippet.publishedAt).toLocaleDateString("vi-VN")}
+              </span>
+            </div>
+            <div class="flex flex-col">
+              <strong class="text-[16px] font-[700]">Languages</strong>
+              <div class="flex gap-2 flex-wrap mt-2">
+                {languages.map((language) => (
+                  <span
+                    class={`${
+                      language.languageCode !== currLanguage
+                        ? "bg-[#D9D9D9] text-black"
+                        : "bg-[#F37B8F26] text-red-600 font-[600]"
+                    } rounded-md px-2 py-1 text-[12px]`}
+                    key={language.languageCode}
+                  >
+                    {language.language}
+                  </span>
+                ))}
+                <span
+                  onClick={() => setOpen(!open)}
+                  class="cursor-pointer relative bg-[#F37B8F] text-white rounded-md px-[10px] flex items-center text-[12px]"
+                >
+                  +
+                  {
+                    /* { remove duplicate} */
+                    open && (
+                      <LanguagesSelection
+                        languages={availableLanguage.filter(
+                          (language) =>
+                            !languages.find(
+                              (l) => l.languageCode === language.languageCode
+                            )
+                        )}
+                        addLanguage={addLanguage}
+                      />
+                    )
+                  }
+                </span>
+              </div>
             </div>
           </div>
         </div>
