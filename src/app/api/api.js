7e5798828cee7da17324fc509bf4772f0f5cd22e
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2024-09-12 15:25:21 +0700

    add channel id through button

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -87,3 +87,40 @@ export const logout = () => {
     console.error("Error occurred during logout:", error.message);
   }
 };
+
+export const addYoutubeChannelID = async (channelId, authToken) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/set_channel_id`,
+      {
+        youtube_channel_id: channelId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Handle the response data
+    const data = response.data;
+    console.log("YouTube Channel Info:", data);
+    return data;
+  } catch (error) {
+    if (error.response) {
+      // Request made and server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error response:", error.response.data);
+      console.error("Error status:", error.response.status);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Error request:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error message:", error.message);
+    }
+    throw error;
+  }
+};
+
