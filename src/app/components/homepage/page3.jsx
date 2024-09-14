b26bb1df10b625cf2ac47f959a96d461ad324c84
dung.do <dung.do@techup.vn>
2024-09-14 17:22:52 +0700

    [add] them duration va music

--- src/app/components/homepage/page3.jsx
+++ src/app/components/homepage/page3.jsx
@@ -1,11 +1,46 @@
 "use client";
 import Banner from "components/common/banner";
 import { useState } from "react";
+import DurationBar from "components/shorts/durationBar";
+
+const musics = [
+  { name: "None", value: "none", src: "" },
+  {
+    name: "Wait wait wait meme",
+    value: "wwwwm",
+    src: "/audios/shorts/wwwww.mp3",
+  },
+  {
+    name: "Có chàng trai viết lên cây",
+    value: "cctvlc",
+    src: "/audios/shorts/cctvlc.mp3",
+  },
+  {
+    name: "Hai Yorokonde",
+    value: "hai_yorokonde",
+    src: "/audios/shorts/hai_yorokonde.mp3",
+  },
+  {
+    name: "Ocean Meme",
+    value: "ocean_meme",
+    src: "/audios/shorts/ocean_meme.mp3",
+  },
+  {
+    name: "Weekend has come",
+    value: "weekend_has_come",
+    src: "/audios/shorts/weekend_has_come.mp3",
+  },
+];
 
 export default function Page3({ data, onChangePage }) {
+  const [duration, setDuration] = useState(10);
+  const [music, setMusic] = useState(musics[0]);
+  const [loadAudio, setLoadAudio] = useState(false);
   const generateShorts = async () => {
     const res = await fetch(`
-      http://127.0.0.1:8000/shorts?url=${`https://www.youtube.com/watch?v=${data.snippet.resourceId.videoId}`}&video_type=${data.type}&video_id=${data.snippet.resourceId.videoId}`);
+      http://127.0.0.1:8000/shorts?url=${`https://www.youtube.com/watch?v=${data.snippet.resourceId.videoId}`}&video_type=${
+      data.type
+    }&video_id=${data.snippet.resourceId.videoId}`);
     const result = await res.json();
     document.getElementById("player").src = result;
   };
@@ -37,7 +72,10 @@ export default function Page3({ data, onChangePage }) {
             allowFullScreen
           ></iframe>
           <div className="flex flex-col gap-4 border-[1px] border-slate-400 w-full h-[80%] overflow-y-auto rounded-lg px-6 py-5">
-            <button onClick={generateShorts} className="w-full rounded-sm text-[#f00] text-[14px] font-[700] py-2 text-center bg-[#F37B8F26]">
+            <button
+              onClick={generateShorts}
+              className="w-full rounded-sm text-[#f00] text-[14px] font-[700] py-2 text-center bg-[#F37B8F26]"
+            >
               Start Generate
             </button>
             <div className="flex flex-col">
@@ -54,7 +92,61 @@ export default function Page3({ data, onChangePage }) {
             </div>
             <div className="flex flex-col">
               <strong className="text-[16px] font-[700]">Duration</strong>
+              <DurationBar duration={duration} />
             </div>
+            <div className="flex items-center gap-2 mt-10">
+              <strong className="text-[16px] font-[700]">Voices</strong>
+              {!loadAudio && music.value !== "none" ? (
+                <button
+                  className="bg-slate-100 size-6 rounded-md p-1"
+                  onClick={() => {
+                    const audio = document.getElementById("audio_player");
+                    audio.play();
+                  }}
+                >
+                  <img src="/images/hear.svg" alt="" className="size-4" />
+                </button>
+              ) : (
+                <img
+                  src="/images/wavesound.gif"
+                  className={`size-4 ${music.value === "none" && "hidden"}`}
+                />
+              )}
+              <audio
+                className="hidden"
+                id="audio_player"
+                src={music.src}
+                onPlay={() => {
+                  setLoadAudio(true);
+                }}
+                onEnded={() => {
+                  setLoadAudio(false);
+                }}
+              />
+            </div>
+            <select
+              className="outline-none"
+              name=""
+              id="char"
+              onChange={() => {
+                setMusic({
+                  value:
+                    musics.find(
+                      (music) =>
+                        music.src === document.getElementById("char").value
+                    )?.value || "none",
+                  src: document.getElementById("char").value || undefined,
+                  name: document.getElementById("char").selectedOptions[0].text,
+                });
+                setLoadAudio(false);
+              }}
+            >
+              {musics.map((music) => (
+                <option key={music.name} value={music.src}>
+                  {music.name}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
--- src/app/components/shorts/durationBar.jsx
+++ src/app/components/shorts/durationBar.jsx
@@ -1,15 +1,36 @@
-export default function DurationBar({ duration }) {
+import React, { useState } from "react";
+
+export default function DurationBar({ setDuration }) {
+  const [hover, setHover] = useState(0);
+  const [durationValue, setDurationValue] = useState(0);
   return (
-    <div className="flex items-center">
-      <div className="w-full h-[4px] bg-slate-200 rounded-lg">
+    <div className="mt-[15px] relative w-[95%]">
+      <hr id="duration_bar" className=" border-black" />
+      {[0, 10, 20, 32, 45, 60].map((item, index) => (
         <div
-          className="h-full bg-slate-600 rounded-lg"
-          style={{ width: duration + "%" }}
-        ></div>
-      </div>
-      <span className="text-slate-600 font-[400] text-[10px] ml-[5px]">
-        {duration}%
-      </span>
+          onMouseEnter={() => setHover(item)}
+          className="relative"
+          key={index}
+          onMouseLeave={() => setHover(durationValue)}
+          onClick={() => {
+            setDurationValue(item);
+          }}
+        >
+          <div
+            key={index}
+            className={`absolute -translate-y-1/2 rounded-full size-4 ${
+              item <= hover ? "bg-[#F37B8F]" : "bg-slate-400"
+            }`}
+            style={{ left: `${(item / 60) * 100}%` }}
+          ></div>
+          <div
+            className={`absolute ${item > 0 ? "left-[-50%]" : "left-0"}`}
+            style={{ left: `${(item / 60) * 100}%`, top: "20px" }}
+          >
+            {item}s
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
