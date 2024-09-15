ed9249c1c4e2ac43b700423e8fc3a3610d8cb185
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2024-09-15 09:42:37 +0700

    translated page and shorts page

---
+++ src/app/(main)/shorts/page.jsx
@@ -0,0 +1,11 @@
+import Banner from "components/common/banner";
+
+export default function ShortsPage() {
+  return (
+    <div>
+      <Banner label="Shorts">
+        <img src="/images/translation_banner.png" alt="" />
+      </Banner>
+    </div>
+  );
+}
---
+++ src/app/(main)/translation/page.jsx
@@ -0,0 +1,9 @@
+import Banner from "components/common/banner";
+
+export default function TranslationPage() {
+  return (
+    <Banner label="Translated Videos">
+      <img src="/images/translation_banner.png" alt="" />
+    </Banner>
+  );
+}
